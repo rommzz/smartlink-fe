8cc92edfb4543f8836e4eaa887ceaef520d97f85
rommzz <rommy_zohara@gmail.com>
2022-02-12 22:03:49 +0700

    format date

--- src/plugins/filter.js
+++ src/plugins/filter.js
@@ -21,3 +21,22 @@ Vue.filter("formatNumber", function(value) {
 	});	
 	return formatter.format(value).toLowerCase().replace('rp', '').replace(',00', '')
 })
+
+Vue.filter("formatDate", function(value) {
+	const month = [
+		'Januari',
+		'Februari',
+		'Maret',
+		'Mei',
+		'Juni',
+		'Juli',
+		'Agustus',
+		'September',
+		'Oktober',
+		'November',
+		'Desember',
+	]
+	const newDate = value.split('-')
+	const bulan = month[parseInt(newDate[1]) - 1]
+	return newDate[0] + ' ' + bulan + ' ' + newDate[2]
+})
